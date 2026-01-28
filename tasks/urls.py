3b81da1e0841fb910e5f285676363c97af4c4e73
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2026-01-28 23:29:19 +0500

    ok

---
+++ tasks/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import home_page, workspace_page, TaksView, TakDetailView
+
+
+urlpatterns = [
+    path('', home_page, name='home'),
+    path('workspace/', workspace_page, name='workspace'),
+
+    # apis
+    path('tasks/', TaksView.as_view(), name='tasks'),
+    path('tasks/<int:id>/', TakDetailView.as_view(), name='task-detail'),
+]
